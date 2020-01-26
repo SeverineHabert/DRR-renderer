787ce51a094655db1a6e2b4a38ac7fbad0b7f765
pangyuteng <pangyuteng@gmail.com>
2020-01-26 13:26:25 -0800

    wip - impl Docker and sk-build logic for installation

---
+++ setup.py
@@ -0,0 +1,13 @@
+import sys
+
+from skbuild import setup
+
+setup(
+    name="DRR-renderer",
+    version="0.1",
+    description="DRR-renderer",
+    author='Severine Habert',
+    license="MIT",
+    packages=["DRR-renderer"],
+    install_requires=[],
+)
