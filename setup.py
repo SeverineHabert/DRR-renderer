31058c954eaa79280332415e5ac13b8c0d838457
pangyuteng <pangyuteng@gmail.com>
2020-01-26 15:45:48 -0800

    added logic to move built executable to bin

--- setup.py
+++ setup.py
@@ -3,11 +3,19 @@ import sys
 from skbuild import setup
 
 setup(
-    name="DRR-renderer",
+    name="DRRgenerator",
     version="0.1",
     description="DRR-renderer",
     author='Severine Habert',
     license="MIT",
-    packages=["DRR-renderer"],
+    packages=[],
     install_requires=[],
 )
+
+# copy built executable to bin folder.
+
+import shutil,os
+src_file = '_skbuild/linux-x86_64-3.7/cmake-install/bin/DRRgenerator'
+tgt_file = 'bin/DRRgenerator'
+if os.path.exists(src_file):
+    shutil.copy(src_file,tgt_file)
